80f118b10899fd5a127021bc73c2e2105538add9
eddgrant <edd@eddgrant.com>
2025-10-29 17:11:35 +0000

    Fix compilation issues (by upgrading pretty much everything!)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "2.1.21"
-    id("com.google.devtools.ksp") version "2.1.20-1.0.32"
+    id("org.jetbrains.kotlin.jvm") version "2.2.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.2.21"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.4"
     id("com.gradleup.shadow") version "9.2.2"
     id("io.micronaut.application") version "4.6.1"
     id("io.micronaut.aot") version "4.6.1"
