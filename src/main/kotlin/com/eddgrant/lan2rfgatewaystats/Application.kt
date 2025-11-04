10eec39b181eb430246b5f6f8e61c153a59e5725
eddgrant <edd@eddgrant.com>
2025-11-04 18:21:32 +0000

    Revert "Merge `StatusDataOrchestrator` functionality into `Application.kt`"
    This reverts commit 0d684364c15af7c2ac84e4aae56ba35325205785.

--- src/main/kotlin/com/eddgrant/lan2rfgatewaystats/Application.kt
+++ src/main/kotlin/com/eddgrant/lan2rfgatewaystats/Application.kt
@@ -1,45 +1,6 @@
 package com.eddgrant.lan2rfgatewaystats
 
-import com.eddgrant.lan2rfgatewaystats.intergas.LAN2RFRepository
-import com.eddgrant.lan2rfgatewaystats.persistence.influxdb.StatusDataPublisher
-import io.micronaut.context.annotation.Context
 import io.micronaut.runtime.Micronaut.run
-import jakarta.annotation.PostConstruct
-import jakarta.annotation.PreDestroy
-import jakarta.inject.Singleton
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import reactor.core.Disposable
-
-@Singleton
-@Context
-class LAN2RFGatewayStatsApp(
-    private val laN2RFRepository: LAN2RFRepository,
-    private val statusDataPublisher: StatusDataPublisher
-) {
-    private lateinit var disposable: Disposable
-
-    @PostConstruct
-    fun start() {
-        LOGGER.info("Starting subscription to LAN2RF data.")
-        disposable = statusDataPublisher.publishAsDiscreteMeasurements(
-            laN2RFRepository.getStatusData()
-        ).subscribe()
-        LOGGER.info("Subscription to LAN2RF data created.")
-    }
-
-    @PreDestroy
-    fun stopSendingStatusData() {
-        LOGGER.info("Shutdown signal received: Cancelling subscription to LAN2RF data.")
-        if (this::disposable.isInitialized && !disposable.isDisposed) {
-            disposable.dispose()
-        }
-    }
-
-    companion object {
-        val LOGGER: Logger = LoggerFactory.getLogger(LAN2RFGatewayStatsApp::class.java)
-    }
-}
 
 fun main(args: Array<String>) {
     run(*args)
